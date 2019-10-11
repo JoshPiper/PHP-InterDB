5e050727e128dc34e6a20536b9f8da17095b44a2
John Internet <jonjon1234.github@gmail.com>
2019-10-11 01:25:29 +0100

    Added MySQL and SQlite drivers.

---
+++ src/Drivers/MySQLDriver.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Internet\InterDB\Drivers;
+
+
+use Internet\InterDB\Exceptions\DSNCreationException;
+
+class MySQLDriver extends AbstractCreatorPDODriver {
+	/** {@inheritDoc}
+	 * @throws DSNCreationException
+	 */
+	function buildDSN(array $settings): string{
+		if (isset($settings['socket'])){
+			unset($settings['host']);
+			unset($settings['port']);
+		} else {
+			self::required($settings, 'host');
+			self::optional($settings, 'port', 3306);
+		}
+
+		$settings['prefix'] = 'mysql';
+		self::remap($settings, 'socket', 'unix_socket');
+		self::remap($settings, 'db', 'dbname');
+
+		return $this->constructDSN($settings);
+	}
+}
