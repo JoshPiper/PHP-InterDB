44a8f1bdee68f0be9125f73ee42390d32306dfc2
John Internet <jonjon1234.github@gmail.com>
2019-10-11 01:23:30 +0100

    Added DSNCreation and SQL Exceptions.

---
+++ src/Exceptions/DSNCreationException.php
@@ -0,0 +1,8 @@
+<?php
+
+
+namespace Internet\InterDB\Exceptions;
+
+use Exception;
+
+class DSNCreationException extends Exception {}
---
+++ src/Exceptions/SQLException.php
@@ -0,0 +1,8 @@
+<?php
+
+
+namespace Internet\InterDB\Exceptions;
+
+use Exception;
+
+class SQLException extends Exception {}
