b5150878976333d688196b5d9dafa2ea8687ee61
John Internet <jonjon1234.github@gmail.com>
2019-10-11 16:45:53 +0100

    Added mariadb for mysql testing.

---
+++ tests/MySQLDriverTest.php
@@ -0,0 +1,36 @@
+<?php
+
+
+use Internet\InterDB\DB;
+use Internet\InterDB\Drivers\MySQLDriver;
+use PHPUnit\Framework\TestCase;
+
+class MySQLDriverTest extends TestCase {
+	/** @var string Test DB file path. */
+	private static $file;
+	/** @var MySQLDriver */
+	private static $driver;
+	/** @var DB */
+	private static $wrapper;
+
+	public static function setUpBeforeClass(): void{
+		self::$file = __DIR__ . '/test.sqlite';
+		if (is_file(self::$file)){
+			unlink(self::$file);
+		}
+
+		self::$driver = new MySQLDriver(['path' => self::$file]);
+		self::$wrapper = new DB(self::$driver);
+	}
+
+	public static function tearDownAfterClass(): void{
+		self::$wrapper = null;
+		self::$driver = null;
+	}
+
+	public function testBuildDSN(){
+		$driver = new MySQLDriver(['host' => 'mariadb'], 'root', 'test_db_password');
+		$this->assertIsString($driver->buildDSN(['host' => 'mariadb']));
+		unset($driver);
+	}
+}
