344698209e889494d2e179a661b81cabc239d14e
John Internet <jonjon1234.github@gmail.com>
2019-10-14 13:54:15 +0100

    why is basic maths so HARD?

--- await-ready.sh
+++ await-ready.sh
@@ -3,7 +3,7 @@ error_count=0
 
 while [ $error_count -lt 20 ]
 do
-	error_count=$[$error_count+1]
+	error_count= $(expr $error_count + 1)
 	mysql --host=mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --execute='SHOW TABLES;'
 	echo $?
 	sleep 2
