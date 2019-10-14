774e6c52ae43661b17e47a4b621398e4d911db90
John Internet <jonjon1234.github@gmail.com>
2019-10-14 15:25:58 +0100

    Added DBWrapper test to test the DB wrapper itself.

---
+++ tests/DBWrapperTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+use Internet\InterDB\DB;
+use PHPUnit\Framework\TestCase;
+use Internet\InterDB\Drivers\TestDriver;
+use Internet\InterDB\Drivers\MySQLDriver;
+use Internet\InterDB\Interfaces\QueryableInterface;
+use Internet\InterDB\Exceptions\DSNCreationException;
+
+final class DBWrapperTest extends TestCase {
+	/** @var DB */
+	private static $wrapper;
+	/** @var QueryableInterface */
+	private static $driver;
+
+	public static function setUpBeforeClass(): void{
+		self::$driver = new MySQLDriver(['host' => 'mysql', 'db' => 'information_schema', 'port' => 3306], 'ci', 'ci');
+		self::$wrapper = new DB(self::$driver);
+	}
+
+	public function testSettingsConstructor(): void{
+		$wrapper = new DB([
+			'host' => 'mysql',
+			'db' => 'information_schema',
+			'port' => 3306,
+			'username' => 'ci',
+			'password' => 'ci'
+		]);
+		$this->assertIsNumeric($wrapper->count('tables'));
+	}
+
+	public function testPDOConstructor(): void{
+		$wrapper = new DB(new MySQLDriver([
+			'host' => 'mysql',
+			'db' => 'information_schema',
+			'port' => 3306
+		], 'ci', 'ci'));
+		$this->assertIsNumeric($wrapper->count('tables'));
+	}
+
+	public function testColumns(): void{
+		$cols = [
+			'keycol' => ['type' => 'bigint', 'ai' => true, 'pk' => true],
+			'namecol' => ['type' => 'varchar', 'length' => '50', 'default' => '"string"']
+		];
+		self::$wrapper->table('testtable', $cols, 'InnoDB');
+	}
+}
