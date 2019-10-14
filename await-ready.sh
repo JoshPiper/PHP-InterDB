20a05ed32e48c833a7236ff732886457990e51d6
John Internet <jonjon1234.github@gmail.com>
2019-10-14 14:17:49 +0100

    Allowed the wait job to fail, fixed some logging and such for the job.

--- await-ready.sh
+++ await-ready.sh
@@ -5,11 +5,16 @@ while [ $error_count -lt 20 ]
 do
 	error_count=$(expr $error_count + 1);
 	echo "Run: $error_count/20";
-	mysql --host=mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --execute='SHOW DATABASES;';
-	echo "Exit Code: $?";
+	mysql --host=mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --execute='SHOW DATABASES;' > /dev/null 2>&1;
 	if [ $? -eq 0 ]; then
 		error_count=999
 	else
 		sleep 2
 	fi
 done
+if [ $error_count -eq 999 ]; then
+	echo "Successfully waited.";
+else
+	echo "Wait failed, expect other jobs to fail.";
+	exit 1
+fi
