94c275c708601910e9baaa9133c0373d5692b542
John Internet <jonjon1234.github@gmail.com>
2019-10-14 13:25:08 +0100

    switch host back to mysql.

--- tests/MySQLDriverTest.php
+++ tests/MySQLDriverTest.php
@@ -7,18 +7,16 @@ use PHPUnit\Framework\TestCase;
 use Internet\InterDB\Exceptions\SQLException;
 
 class MySQLDriverTest extends TestCase {
-	/** @var string Test DB file path. */
-	private static $file;
 	/** @var MySQLDriver */
 	private static $driver;
 	/** @var DB */
 	private static $wrapper;
 
 	public static function setUpBeforeClass(): void{
-		$retries = 10;
+		$retries = 20;
 		do {
 			try {
-				self::$driver = new MySQLDriver(['host' => 'mariadb', 'db' => 'information_schema', 'port' => 3306], 'root', 'ci');
+				self::$driver = new MySQLDriver(['host' => 'mysql', 'db' => 'information_schema', 'port' => 3306], 'root', 'ci');
 				self::$wrapper = new DB(self::$driver);
 			} catch (SQLException $exception){
 				$retries--;
