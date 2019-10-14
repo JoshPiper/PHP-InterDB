1ce658ed93a12d70573ea4695c060d3fc604181e
John Internet <jonjon1234.github@gmail.com>
2019-10-14 14:13:38 +0100

    Check for success for early break.

--- await-ready.sh
+++ await-ready.sh
@@ -3,9 +3,13 @@ error_count=0
 
 while [ $error_count -lt 20 ]
 do
-	error_count=$(expr $error_count + 1)
-	echo "Run: $error_count/20"
-	mysql --host=mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --execute='SHOW TABLES;'
-	echo "Exit Code: $?"
-	sleep 2
+	error_count=$(expr $error_count + 1);
+	echo "Run: $error_count/20";
+	mysql --host=mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --execute='SHOW DATABASES;';
+	echo "Exit Code: $?";
+	if [ $? -eq 0 ]; then
+		error_count=999
+	else
+		sleep 2
+	fi
 done
