60a5dce8b2afb01bbd8c8a5cc62ec97b56d7e91a
John Internet <jonjon1234.github@gmail.com>
2019-10-14 13:51:24 +0100

    reee.

--- await-ready.sh
+++ await-ready.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 error_count=0
 
-while [ $error_count -lt 20 -a $? -ne 0 ]
+while [ $error_count -lt 20 ]
 do
 	error_count=$[$error_count+1]
 	mysql --host=mysql --user=root --password="$MYSQL_ROOT_PASSWORD" --execute='SHOW TABLES;'
+	echo $?
 	sleep 2
 done
