626a1999803bf66c692e68aca0fe167f3e80576c
John Internet <jonjon1234.github@gmail.com>
2019-10-18 09:56:04 +0100

    Add the new line after the comma.

--- src/Definers/MySQLTableDefiner.php
+++ src/Definers/MySQLTableDefiner.php
@@ -6,8 +6,8 @@ namespace Internet\InterDB\Definers;
 
 class MySQLTableDefiner extends AbstractTableDefiner {
 	public function toSQL(): string{
-		$data = join("\n,\t", $this->getColumnDefs());
-		$data = "CREATE TABLE `{$this->schema}`.`{$this->table}` (\n{$data}\n)";
+		$data = join(",\n\t", $this->getColumnDefs());
+		$data = "CREATE TABLE `{$this->schema}`.`{$this->table}` (\n\t{$data}\n)";
 		if ($this->engine){$data .= " ENGINE={$this->engine}";}
 		$data .= ';';
 
--- src/Definers/SQLiteTableDefiner.php
+++ src/Definers/SQLiteTableDefiner.php
@@ -6,7 +6,7 @@ namespace Internet\InterDB\Definers;
 
 class SQLiteTableDefiner extends AbstractTableDefiner {
 	public function toSQL(): string{
-		$data = join("\n,\t", $this->getColumnDefs());
-		return "CREATE TABLE `{$this->table}` (\n{$data}\n);";
+		$data = join(",\n\t", $this->getColumnDefs());
+		return "CREATE TABLE `{$this->table}` (\n\t{$data}\n);";
 	}
 }
