b4e94c0f197087349adb9dc113183463f4714fbe
John Internet <jonjon1234.github@gmail.com>
2019-10-18 11:33:59 +0100

    var dump

--- src/Definers/SQLiteTableDefiner.php
+++ src/Definers/SQLiteTableDefiner.php
@@ -6,7 +6,9 @@ namespace Internet\InterDB\Definers;
 
 class SQLiteTableDefiner extends AbstractTableDefiner {
 	public function toSQL(): string{
-		$data = join(",\n\t", $this->getColumnDefs(false));
-		return "CREATE TABLE `{$this->table}` (\n\t{$data}\n);";
+		$data = join(",\n\t", $this->getColumnDefs());
+		$data = "CREATE TABLE `{$this->table}` (\n\t{$data}\n);";
+		var_dump($data);
+		return $data;
 	}
 }
