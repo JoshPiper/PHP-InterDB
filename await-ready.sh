052a6dca586b6d51f83af5e6a274063bf0c16fb9
John Internet <jonjon1234.github@gmail.com>
2020-03-11 14:47:05 +0000

    whoops.

--- await-ready.sh
+++ await-ready.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
+max_counts=30
 error_count=0
 
-while [ $error_count -lt 30 ]
+while [ $error_count -lt $max_counts ]
 do
 	error_count=$(expr $error_count + 1);
-	echo "Run: $error_count/20";
+	echo "Run: $error_count/$max_counts";
 	mysql --host=mysql --port="$MYSQL_PORT" --user=root --password="$MYSQL_PASSWORD" --execute='SHOW DATABASES;' > /dev/null;
 	if [ $? -eq 0 ]; then
 		error_count=999
