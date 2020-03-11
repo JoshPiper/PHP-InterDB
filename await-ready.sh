d686aa59c592f275c0f25935f7a4b67abb41b8df
John Internet <jonjon1234.github@gmail.com>
2020-03-11 14:52:22 +0000

    pls github work

--- await-ready.sh
+++ await-ready.sh
@@ -2,10 +2,13 @@
 max_counts=30
 error_count=0
 
+echo "GitHub reports service port as $MYSQL_PORT"
 while [ $error_count -lt $max_counts ]
 do
 	error_count=$(expr $error_count + 1);
 	echo "Run: $error_count/$max_counts";
+	mysql --host=localhost --port="$MYSQL_PORT" --user=root --password="$MYSQL_PASSWORD" --execute='SHOW DATABASES;' > /dev/null;
+	mysql --host=mysql --port="$MYSQL_PORT" --user=root --password="$MYSQL_PASSWORD" --execute='SHOW DATABASES;' > /dev/null;
 	mysql --host=127.0.0.1 --port="$MYSQL_PORT" --user=root --password="$MYSQL_PASSWORD" --execute='SHOW DATABASES;' > /dev/null;
 	if [ $? -eq 0 ]; then
 		error_count=999
